2a8a64799ff5e970eb42785d570269c457fd22ab
北城白衣 <sszyc8@qq.com>
2024-11-12 09:35:02 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'sszyc8.github.io',
+    site: 'https://sszyc8.github.io',
 });
